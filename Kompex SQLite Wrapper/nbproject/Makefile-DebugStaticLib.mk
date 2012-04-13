5394d0d75067eedab9cd31d31b12844b7f8c12b7
Geoffroy PLANQUART <geoffroy@aethelflaed.com>
2012-04-13 22:20:58 +0200

    Bugfix in variable name

--- Kompex SQLite Wrapper/nbproject/Makefile-DebugStaticLib.mk
+++ Kompex SQLite Wrapper/nbproject/Makefile-DebugStaticLib.mk
@@ -65,17 +65,17 @@ LDLIBSOPTIONS=
 ${OBJECTDIR}/_ext/_DOTDOT/src/KompexSQLiteBlob.o: nbproject/Makefile-${CND_CONF}.mk ../src/KompexSQLiteBlob.cpp 
 	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/src
 	${RM} $@.d
-	$(COMPILE.cc) -g -I${INCLUDE_DIRECTORY} -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/_DOTDOT/src/KompexSQLiteBlob.o ../src/KompexSQLiteBlob.cpp
+	$(COMPILE.cc) -g -I${INCLUDE_DIR} -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/_DOTDOT/src/KompexSQLiteBlob.o ../src/KompexSQLiteBlob.cpp
 
 ${OBJECTDIR}/_ext/_DOTDOT/src/KompexSQLiteStatement.o: nbproject/Makefile-${CND_CONF}.mk ../src/KompexSQLiteStatement.cpp 
 	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/src
 	${RM} $@.d
-	$(COMPILE.cc) -g -I${INCLUDE_DIRECTORY} -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/_DOTDOT/src/KompexSQLiteStatement.o ../src/KompexSQLiteStatement.cpp
+	$(COMPILE.cc) -g -I${INCLUDE_DIR} -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/_DOTDOT/src/KompexSQLiteStatement.o ../src/KompexSQLiteStatement.cpp
 
 ${OBJECTDIR}/_ext/_DOTDOT/src/KompexSQLiteDatabase.o: nbproject/Makefile-${CND_CONF}.mk ../src/KompexSQLiteDatabase.cpp 
 	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/src
 	${RM} $@.d
-	$(COMPILE.cc) -g -I${INCLUDE_DIRECTORY} -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/_DOTDOT/src/KompexSQLiteDatabase.o ../src/KompexSQLiteDatabase.cpp
+	$(COMPILE.cc) -g -I${INCLUDE_DIR} -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/_DOTDOT/src/KompexSQLiteDatabase.o ../src/KompexSQLiteDatabase.cpp
 
 ${OBJECTDIR}/_ext/_DOTDOT/src/sqlite3.o: nbproject/Makefile-${CND_CONF}.mk ../src/sqlite3.c 
 	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/src
