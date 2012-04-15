ca9871762853a93bab74af2323310ed5a1a8631b
Geoffroy PLANQUART <geoffroy@planquart.fr>
2012-04-15 09:41:09 +0200

    Added lib prefix to library

--- Makefile-shared.mk
+++ Makefile-shared.mk
@@ -21,15 +21,15 @@ CPPFLAGS= -DKOMPEX_SQLITEWRAPPER_EXPORT -DKOMPEX_SQLITEWRAPPER_DYN -fPIC -MMD -M
 LDLIBSOPTIONS= -shared -fPIC
 
 # Build Targets
-.build-conf: .pre-build ${prelibdir}/${PRODUCT_NAME}.so
+.build-conf: .pre-build ${prelibdir}/lib${PRODUCT_NAME}.so
 
 .pre-build:
 	$(MKDIR) -p ${prelibdir}
 	$(MKDIR) -p ${objsdir}
 	$(RM) ${objsdir}/*.d
 
-${prelibdir}/${PRODUCT_NAME}.so: ${OBJS}
-	${LINK.cc} -o ${prelibdir}/${PRODUCT_NAME}.so ${OBJS} ${LDLIBSOPTIONS} 
+${prelibdir}/lib${PRODUCT_NAME}.so: ${OBJS}
+	${LINK.cc} -o ${prelibdir}/lib${PRODUCT_NAME}.so ${OBJS} ${LDLIBSOPTIONS} 
 
 ${objsdir}/KompexSQLiteBlob.o: ${srcdir}/KompexSQLiteBlob.cpp 
 	$(COMPILE.cc) ${CXXFLAGS} -MF $@.d -o $@ $^
--- Makefile-static.mk
+++ Makefile-static.mk
@@ -21,17 +21,17 @@ CPPFLAGS= -I${includedir} -MMD -MP
 LDLIBSOPTIONS=
 
 # Build Targets
-.build-conf: .pre-build ${prelibdir}/${PRODUCT_NAME}.a
+.build-conf: .pre-build ${prelibdir}/lib${PRODUCT_NAME}.a
 
 .pre-build:
 	$(MKDIR) -p ${prelibdir}
 	$(MKDIR) -p ${objsdir}
 	$(RM) ${objsdir}/*.d
-	$(RM) ${prelibdir}/${PRODUCT_NAME}.a
+	$(RM) ${prelibdir}/lib${PRODUCT_NAME}.a
 
-${prelibdir}/${PRODUCT_NAME}.a: ${OBJS}
-	${AR} rv ${prelibdir}/${PRODUCT_NAME}.a ${OBJS} 
-	$(RANLIB) ${prelibdir}/${PRODUCT_NAME}.a
+${prelibdir}/lib${PRODUCT_NAME}.a: ${OBJS}
+	${AR} rv ${prelibdir}/lib${PRODUCT_NAME}.a ${OBJS} 
+	$(RANLIB) ${prelibdir}/lib${PRODUCT_NAME}.a
 
 ${objsdir}/KompexSQLiteBlob.o: ${srcdir}/KompexSQLiteBlob.cpp 
 	$(COMPILE.cc) -MF $@.d -o $@ $^
